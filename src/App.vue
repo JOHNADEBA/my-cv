25be10587e2503767c074d4db03d0511d5e6a009
john.adeba <John.Adeba@endava.com>
2022-11-29 10:52:48 +0100

    remove comments

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <!-- <button @click="downloadWithCSS">Download PDF WITH CSS</button> -->
-
-    <HeaderCv />
-    <SummarySkills />
-    <ExperienceCv />
-    <EducationCv />
-  </div>
+	<div id="app">
+		<HeaderCv />
+		<SummarySkills />
+		<ExperienceCv />
+		<EducationCv />
+	</div>
 </template>
 
 <script>
@@ -18,68 +16,46 @@ import ExperienceCv from "./components/ExperienceCv.vue";
 import EducationCv from "./components/EducationCv.vue";
 
 export default {
-  name: "App",
-  components: {
-    HeaderCv,
-    SummarySkills,
-    ExperienceCv,
-    EducationCv,
-  },
+	name: "App",
+	components: {
+		HeaderCv,
+		SummarySkills,
+		ExperienceCv,
+		EducationCv,
+	},
 
-  mounted() {
-    // this.downloadWithCSS();
-  },
-  methods: {
-    // downloadWithCSS() {
-    //   html2pdf(this.$refs.document, {
-    //     margin: 1,
-    //     filename: "cv.pdf",
-    //     image: { type: "jpeg", quality: 0.98 },
-    //     html2canvas:{ dpi: 192, letterRendering: true },
-    //     jsPDF:{ unit: "in", format:'letter', orientation:'landscape' },
-    //   });
-    // },
-    // downloadWithCSS() {
-    //   const doc = new jsPDF();
-    //   /** WITH CSS */
-    //   var canvasElement = document.createElement("canvas");
-    //   html2canvas(this.$refs.content, { canvas: canvasElement }).then(function (
-    //     canvas
-    //   ) {
-    //     const img = canvas.toDataURL("image/jpeg", 0.8);
-    //     doc.addImage(img, "JPEG", 20, 20);
-    //     doc.save("john.pdf");
-    //   });
-    // },
-    downloadWithCSS() {
-      window.html2canvas = html2canvas;
-      var doc = new jsPDF("p", "pt", "a4");
-      doc.html(document.querySelector("#app"), {
-        callback: function (pdf) {
-          pdf.save("mycv.pdf")
-        }
-      });
-    },
-  },
+	mounted() {
+		// this.downloadWithCSS();
+	},
+	methods: {
+		downloadWithCSS() {
+			window.html2canvas = html2canvas;
+			var doc = new jsPDF("p", "pt", "a4");
+			doc.html(document.querySelector("#app"), {
+				callback: function (pdf) {
+					pdf.save("mycv.pdf");
+				},
+			});
+		},
+	},
 };
 </script>
 
 <style>
 #app {
-  /* width: 90%; */
-  margin: auto;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  padding-bottom: 1rem;
-  border-radius: 0.3rem;
-  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
-  /* text-align: center;
+	/* width: 90%; */
+	margin: auto;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	padding-bottom: 1rem;
+	border-radius: 0.3rem;
+	box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+	/* text-align: center;
   color: #2c3e50;
   margin-top: 60px; */
 
-
-  height: 11in;  
-  width: 8.5in;
+	height: 11in;
+	width: 8.5in;
 }
 </style>
